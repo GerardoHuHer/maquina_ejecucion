fa1b1553ae2864ce8e68e98114ca95662ee34d01
GerardoHuHer <gerardohuerta1502@gmail.com>
2025-08-17 23:04:40 -0600

    feat: Se añaden validaciones para cantidad de argumentos pasados por terminal, así como instanciar el array de las instrucciones

--- main.cpp
+++ main.cpp
@@ -1,5 +1,32 @@
 #include "maquina_ejecucion.h"
 
-int main(){
+int main(int argc, char** argv) {
+    // validamos si el usuario paso el path del archivo desde terminal.
+    if (argc < 2) {
+        std::cerr << "Por favor, proporciona el archivo de instrucciones." << std::endl;
+        return 1;
+    }
+
+    Instruccion* instrucciones= new Instruccion[INSTRUCCIONES_MAXIMO];
+    for(unsigned int i = 0; i < INSTRUCCIONES_MAXIMO; i++){
+        instrucciones[i].comando = HALT;
+        instrucciones[i].no_linea = i;
+        instrucciones[i].r = 0;
+        instrucciones[i].d = 0;
+        instrucciones[i].s = 0;
+        instrucciones[i].t = 0;
+    }
+
+    lexer(instrucciones, argv[1]);
+
+    for(unsigned int i = 0; i < 10; i++){
+        // Imprimir solo las instrucciones válidas
+        if (instrucciones[i].comando != NONE) {
+            std::cout << "Instrucción en línea " << i << ": " << instrucciones[i].comando << std::endl;
+        }
+    }
+
+    delete[] instrucciones;
+
     return 0;
 }
