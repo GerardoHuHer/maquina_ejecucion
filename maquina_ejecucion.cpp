b129b7ada3e32fb7852d6944976b0d828c32cfb9
GerardoHuHer <gerardohuerta1502@gmail.com>
2025-08-22 16:34:29 -0600

    feat: Se desarrolla la función separar_variables para obtener r,s,t y d, falta arreglar algunos casos descritos con el comentario FIX

--- maquina_ejecucion.cpp
+++ maquina_ejecucion.cpp
@@ -22,6 +22,85 @@ TipoComando stringToTipoComando(const std::string& command) {
     
 }
 
+// FIX: Verificar espacios en blanco en los registros para que no marque error al usar función stoi
+// 6,0,0 funciona, pero 6, 0, 0 no
+std::string remove_whitespace(const std::string& str) {
+    std::string aux;
+    unsigned int i = 0;
+    while(str[i] != '\0'){
+        if(str[i] != ' ' && str[i] != '\t'){
+            aux += str[i];
+        }
+        i++;
+    }
+    return aux;
+}
+
+
+// Función que recibe las variables r, s, d, t, la cadena con sus valores y la instruccion para saber como dividirlos
+void separar_variables( TipoComando instruccion, int16_t& r, int16_t& s, int16_t& t, int16_t& d, const std::string& operand_str) {
+    // Para instrucción HALT donde inicializamos todos los valores en 0
+    if(instruccion == HALT){
+        r = 0; s = 0; t = 0; d = 0; 
+    }    
+    std::stringstream ss(operand_str);
+    std::string temp_str;
+    char comma, parentesis;
+
+    switch (instruccion) {
+        case HALT:
+            r = 0; s = 0; t = 0; d = 0;
+            break;
+
+        case IN:
+        case OUT:
+            // Lee la cadena, ignora los espacios y convierte a int.
+            ss >> temp_str;
+            r = static_cast<int16_t>(std::stoi(remove_whitespace(temp_str)));
+            s = 0; t = 0; d = 0;
+            break;
+
+        case ADD:
+        case SUB:
+        case MUL:
+        case DIV:
+            // Lee r, s y t, ignorando las comas.
+            std::getline(ss, temp_str, ',');
+            r = static_cast<int16_t>(std::stoi(remove_whitespace(temp_str)));
+            std::getline(ss, temp_str, ',');
+            s = static_cast<int16_t>(std::stoi(remove_whitespace(temp_str)));
+            std::getline(ss, temp_str); // Lee hasta el final de la línea
+            t = static_cast<int16_t>(std::stoi(remove_whitespace(temp_str)));
+            d = 0;
+            break;
+
+        case LD:
+        case LDA:
+        case LDC:
+        case ST:
+        case JLT:
+        case JLE:
+        case JGE:
+        case JGT:
+        case JEQ:
+        case JNE:
+            // Lee r, d, y s.
+            std::getline(ss, temp_str, ',');
+            r = static_cast<int16_t>(std::stoi(remove_whitespace(temp_str)));
+            std::getline(ss, temp_str, '('); // Lee hasta el paréntesis
+            d = static_cast<int16_t>(std::stoi(remove_whitespace(temp_str)));
+            std::getline(ss, temp_str, ')'); // Lee el valor de 's'
+            s = static_cast<int16_t>(std::stoi(remove_whitespace(temp_str)));
+            t = 0;
+            break;
+
+        default:
+            r = 0; s = 0; t = 0; d = 0;
+            break;        
+    }       
+    
+}
+
 /*Función para leer el archivo y almacenar las instrucciones en el arreglo instrucciones*/
 void lexer(Instruccion* instrucciones, const std::string& path) {
     std::ifstream inputFile(path);
@@ -37,27 +116,31 @@ void lexer(Instruccion* instrucciones, const std::string& path) {
         unsigned int first_char_pos = line.find_first_not_of(" \t\n\r");
 
         // FIX: Arreglar si detecta una línea en blanco saltar
-        if (first_char_pos == std::string::npos || line[first_char_pos] == '*') {
+        if (first_char_pos == std::string::npos || line[first_char_pos] == '*' ) {
             continue;
         }
+
+        // Transformamos la línea en stringstream 
         std::stringstream ss(line);
         std::string num_str;
         std::getline(ss, num_str, ':'); // Leemos el número de línea
         int16_t no_linea = std::stoi(num_str);
         instrucciones[no_linea].no_linea = no_linea;
 
-         if (no_linea >= 0 && no_linea < INSTRUCCIONES_MAXIMO) {
+        if (no_linea >= 0 && no_linea < INSTRUCCIONES_MAXIMO) {
             instrucciones[no_linea].no_linea = no_linea;
 
             std::string command_str;
             ss >> command_str;
             instrucciones[no_linea].comando = stringToTipoComando(command_str);
 
-            // TODO: Lectura y almacenamiento de r, s, t y d
+
+            std::string operand_str;
+            ss >> operand_str;
+
+            separar_variables(instrucciones[no_linea].comando, instrucciones[no_linea].r, instrucciones[no_linea].s, instrucciones[no_linea].t, instrucciones[no_linea].d, operand_str);
+
             continue;
-
-
-
         }
          else {
             std::cerr << "Número de línea fuera de rango: " << no_linea << std::endl;
