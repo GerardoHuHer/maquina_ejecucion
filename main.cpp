75aad33ed36c2b8861d7cd69a970cf7c820c828c
GerardoHuHer <gerardohuerta1502@gmail.com>
2025-08-26 07:15:27 -0600

    fix: Se cambia nombre de las instrucciones para ser instrucciones_memoria

--- main.cpp
+++ main.cpp
@@ -7,34 +7,34 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    Instruccion* instrucciones= new Instruccion[INSTRUCCIONES_MAXIMO];
+    Instruccion* instrucciones_memoria= new Instruccion[INSTRUCCIONES_MAXIMO];
     for(unsigned int i = 0; i < INSTRUCCIONES_MAXIMO; i++){
-        instrucciones[i].comando = HALT;
-        instrucciones[i].no_linea = i;
-        instrucciones[i].r = 0;
-        instrucciones[i].d = 0;
-        instrucciones[i].s = 0;
-        instrucciones[i].t = 0;
+        instrucciones_memoria[i].comando = HALT;
+        instrucciones_memoria[i].no_linea = i;
+        instrucciones_memoria[i].r = 0;
+        instrucciones_memoria[i].d = 0;
+        instrucciones_memoria[i].s = 0;
+        instrucciones_memoria[i].t = 0;
     }
 
-    lexer(instrucciones, argv[1]);
+    lexer(instrucciones_memoria, argv[1]);
+    std::sort(instrucciones_memoria, instrucciones_memoria + INSTRUCCIONES_MAXIMO, [](const Instruccion& a, const Instruccion& b) {
+        return a.no_linea < b.no_linea;
+    });
 
-    
 
     unsigned int i = 0;
     do{
-        std::cout << "Instrucción en línea " << i << ": " << instrucciones[i].comando;
-        std::cout  << " r: " << instrucciones[i].r 
-                   << " s: " << instrucciones[i].s 
-                   << " t: " << instrucciones[i].t 
-                   << " d: " << instrucciones[i].d 
+        std::cout << "Instrucción en línea " << i << ": " << instrucciones_memoria[i].comando;
+        std::cout  << " r: " << instrucciones_memoria[i].r
+                   << " s: " << instrucciones_memoria[i].s
+                   << " t: " << instrucciones_memoria[i].t
+                   << " d: " << instrucciones_memoria[i].d
                    << std::endl;
         i++;
-    }while (instrucciones[i - 1].comando != HALT);
-    
-   
+    }while (instrucciones_memoria[i - 1].comando != HALT);
 
-    delete[] instrucciones;
+    delete[] instrucciones_memoria;
 
     return 0;
 }
