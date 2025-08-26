6d4468e4875fbb606cb064a4159f85a31be0a50e
GerardoHuHer <gerardohuerta1502@gmail.com>
2025-08-26 07:16:47 -0600

    fix: Se elimina la función whitespace no es funcional y hay que verificar try catch para la lectura en caso de haber errores

--- maquina_ejecucion.cpp
+++ maquina_ejecucion.cpp
@@ -18,24 +18,8 @@ TipoComando stringToTipoComando(const std::string& command) {
     if (command == "JGT") return JGT;
     if (command == "JEQ") return JEQ;
     if (command == "JNE") return JNE;
-    return NONE;
-    
+    return HALT;
 }
-
-// FIX: Verificar espacios en blanco en los registros para que no marque error al usar función stoi
-// 6,0,0 funciona, pero 6, 0, 0 no
-std::string remove_whitespace(const std::string& str) {
-    std::string aux;
-    unsigned int i = 0;
-    while(str[i] != '\0'){
-        if(str[i] != ' ' && str[i] != '\t'){
-            aux += str[i];
-        }
-        i++;
-    }
-    return aux;
-}
-
 
 // Función que recibe las variables r, s, d, t, la cadena con sus valores y la instruccion para saber como dividirlos
 void separar_variables( TipoComando instruccion, int16_t& r, int16_t& s, int16_t& t, int16_t& d, const std::string& operand_str) {
@@ -51,12 +35,11 @@ void separar_variables( TipoComando instruccion, int16_t& r, int16_t& s, int16_t
         case HALT:
             r = 0; s = 0; t = 0; d = 0;
             break;
-
         case IN:
         case OUT:
             // Lee la cadena, ignora los espacios y convierte a int.
             ss >> temp_str;
-            r = static_cast<int16_t>(std::stoi(remove_whitespace(temp_str)));
+            r = static_cast<int16_t>(std::stoi(temp_str));
             s = 0; t = 0; d = 0;
             break;
 
@@ -66,14 +49,13 @@ void separar_variables( TipoComando instruccion, int16_t& r, int16_t& s, int16_t
         case DIV:
             // Lee r, s y t, ignorando las comas.
             std::getline(ss, temp_str, ',');
-            r = static_cast<int16_t>(std::stoi(remove_whitespace(temp_str)));
+            r = static_cast<int16_t>(std::stoi(temp_str));
             std::getline(ss, temp_str, ',');
-            s = static_cast<int16_t>(std::stoi(remove_whitespace(temp_str)));
+            s = static_cast<int16_t>(std::stoi(temp_str));
             std::getline(ss, temp_str); // Lee hasta el final de la línea
-            t = static_cast<int16_t>(std::stoi(remove_whitespace(temp_str)));
+            t = static_cast<int16_t>(std::stoi(temp_str));
             d = 0;
             break;
-
         case LD:
         case LDA:
         case LDC:
@@ -86,11 +68,11 @@ void separar_variables( TipoComando instruccion, int16_t& r, int16_t& s, int16_t
         case JNE:
             // Lee r, d, y s.
             std::getline(ss, temp_str, ',');
-            r = static_cast<int16_t>(std::stoi(remove_whitespace(temp_str)));
+            r = static_cast<int16_t>(std::stoi((temp_str)));
             std::getline(ss, temp_str, '('); // Lee hasta el paréntesis
-            d = static_cast<int16_t>(std::stoi(remove_whitespace(temp_str)));
+            d = static_cast<int16_t>(std::stoi((temp_str)));
             std::getline(ss, temp_str, ')'); // Lee el valor de 's'
-            s = static_cast<int16_t>(std::stoi(remove_whitespace(temp_str)));
+            s = static_cast<int16_t>(std::stoi((temp_str)));
             t = 0;
             break;
 
@@ -98,7 +80,6 @@ void separar_variables( TipoComando instruccion, int16_t& r, int16_t& s, int16_t
             r = 0; s = 0; t = 0; d = 0;
             break;        
     }       
-    
 }
 
 /*Función para leer el archivo y almacenar las instrucciones en el arreglo instrucciones*/
@@ -111,42 +92,56 @@ void lexer(Instruccion* instrucciones, const std::string& path) {
     } 
 
     std::string line;
+    unsigned int index = 0;
     while (std::getline(inputFile, line)) {
+
+
+        if(std::all_of(line.begin(), line.end(), [](unsigned char c) {
+            return std::isspace(c);
+        })) {
+            continue;
+        }
+
         // Encontrar la posición del primer carácter no-espacio.
         unsigned int first_char_pos = line.find_first_not_of(" \t\n\r");
 
-        // FIX: Arreglar si detecta una línea en blanco saltar
+
         if (first_char_pos == std::string::npos || line[first_char_pos] == '*' ) {
             continue;
         }
 
-        // Transformamos la línea en stringstream 
-        std::stringstream ss(line);
-        std::string num_str;
-        std::getline(ss, num_str, ':'); // Leemos el número de línea
-        int16_t no_linea = std::stoi(num_str);
-        instrucciones[no_linea].no_linea = no_linea;
+        char first_char = line[first_char_pos];
 
-        if (no_linea >= 0 && no_linea < INSTRUCCIONES_MAXIMO) {
-            instrucciones[no_linea].no_linea = no_linea;
+        if(std::isdigit(static_cast<unsigned char>(first_char))){
+            
+            std::stringstream ss(line);
+            std::string num_str;
+            // Transformamos la línea en stringstream
+            std::getline(ss, num_str, ':'); // Leemos el número de línea
+            int16_t no_linea = static_cast<int16_t>(std::stoi(num_str));
+            instrucciones[index].no_linea = no_linea;
+            if (no_linea >= 0 && no_linea < INSTRUCCIONES_MAXIMO) {
+                instrucciones[index].no_linea = no_linea;
 
-            std::string command_str;
-            ss >> command_str;
-            instrucciones[no_linea].comando = stringToTipoComando(command_str);
+                std::string command_str;
+                ss >> command_str;
+                instrucciones[index].comando = stringToTipoComando(command_str);
 
 
-            std::string operand_str;
-            ss >> operand_str;
+                std::string operand_str;
+                ss >> operand_str;
 
-            separar_variables(instrucciones[no_linea].comando, instrucciones[no_linea].r, instrucciones[no_linea].s, instrucciones[no_linea].t, instrucciones[no_linea].d, operand_str);
+                separar_variables(instrucciones[index].comando, instrucciones[index].r, instrucciones[index].s, instrucciones[index].t, instrucciones[index].d, operand_str);
+                index++;
 
-            continue;
-        }
-         else {
-            std::cerr << "Número de línea fuera de rango: " << no_linea << std::endl;
-        
+                continue;
+            } else {
+                std::cerr << "Número de línea fuera de rango: " << no_linea << std::endl;
+            }
+        } else {
+            std::cout << "error: la línea inicia con un caracter inválido: " << line << std::endl;
+            std::exit(EXIT_FAILURE);
         }
     }
-
     inputFile.close();
 }
