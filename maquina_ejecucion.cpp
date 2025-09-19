78f99ab0978e02117d9dcde9aaccef146eda96dc
GerardoHuHer <gerardohuerta1502@gmail.com>
2025-09-19 16:39:57 -0600

    feat: Se añaden validaciones al pasar de string a int en la lectura y al ingresar un valor

--- maquina_ejecucion.cpp
+++ maquina_ejecucion.cpp
@@ -44,10 +44,11 @@ void separar_variables( TipoComando instruccion, int& r, int& s, int& t, int& d,
         case OUT:
             // Lee la cadena, ignora los espacios y convierte a int.
             ss >> temp_str;
-            try {
-                r = std::stoi(temp_str);
-            } catch (const std::invalid_argument& e) {
-                std::cerr << "Error: valor inválido para r en la línea " << linea << std::endl;
+            try{
+
+            r = stoi(temp_str); 
+            } catch(const std::invalid_argument& e){
+                std::cerr << "Error: Operando inválido para la instrucción en línea " << linea << std::endl;
                 std::exit(EXIT_FAILURE);
             }
             s = 0; t = 0; d = 0;
@@ -59,11 +60,27 @@ void separar_variables( TipoComando instruccion, int& r, int& s, int& t, int& d,
         case DIV:
             // Lee r, s y t, ignorando las comas.
             std::getline(ss, temp_str, ',');
-            r = std::stoi(temp_str);
+            try{
+
+            r = stoi(temp_str);
+            } catch(const std::invalid_argument& e){
+                std::cerr << "Error: Operando inválido para la instrucción en línea " << linea << std::endl;
+                std::exit(EXIT_FAILURE);
+            }
             std::getline(ss, temp_str, ',');
-            s = std::stoi(temp_str);
+            try{
+                s = stoi(temp_str);
+            } catch(const std::invalid_argument& e){
+                std::cerr << "Error: Operando inválido para la instrucción en línea " << linea << std::endl;
+                std::exit(EXIT_FAILURE);
+            }
             std::getline(ss, temp_str); // Lee hasta el final de la línea
-            t = std::stoi(temp_str);
+            try{
+                t = stoi(temp_str);
+            } catch(const std::invalid_argument& e){
+                std::cerr << "Error: Operando inválido para la instrucción en línea " << linea << std::endl;
+                std::exit(EXIT_FAILURE);
+            }
             d = 0;
             break;
         case LD:
@@ -78,17 +95,30 @@ void separar_variables( TipoComando instruccion, int& r, int& s, int& t, int& d,
         case JNE:
             // Lee r, d, y s.
             std::getline(ss, temp_str, ',');
-            r = std::stoi((temp_str));
+            try{
+                r = stoi(temp_str);
+            } catch(const std::invalid_argument& e){
+                std::cerr << "Error: Operando inválido para la instrucción en línea " << linea << std::endl;
+                std::exit(EXIT_FAILURE);
+            }
+
             std::getline(ss, temp_str, '('); // Lee hasta el paréntesis
-            d = std::stoi((temp_str));
+            try {
+                d = std::stoi((temp_str));
+            } catch(const std::invalid_argument& e){
+                std::cerr << "Error: Operando inválido para la instrucción en línea " << linea << std::endl;
+                std::exit(EXIT_FAILURE);
+            }
             std::getline(ss, temp_str, ')'); // Lee el valor de 's'
-            s = std::stoi((temp_str));
+            try{
+                s = std::stoi(temp_str);
+            } catch(const std::invalid_argument& e){
+                std::cerr << "Error: Operando inválido para la instrucción en línea " << linea << std::endl;
+                std::exit(EXIT_FAILURE);
+            }
             t = 0;
             break;
-
-        default:
-            r = 0; s = 0; t = 0; d = 0;
-            break;        
+             
     }       
 }
 
@@ -105,6 +135,7 @@ void lexer(Instruccion* instrucciones, const std::string& path) {
     while (std::getline(inputFile, line)) {
 
 
+        // Validación con función lambda para líneas en blanco
         if(std::all_of(line.begin(), line.end(), [](unsigned char c) {
             return std::isspace(c);
         })) {
@@ -112,7 +143,9 @@ void lexer(Instruccion* instrucciones, const std::string& path) {
         }
 
         // Encontrar la posición del primer carácter no-espacio.
+        // Devuelve un npos si la línea está vacía o contiene solo espacios.
         unsigned int first_char_pos = line.find_first_not_of(" \t\n\r");
+
 
 
         if (first_char_pos == std::string::npos || line[first_char_pos] == '*' ) {
@@ -123,22 +156,22 @@ void lexer(Instruccion* instrucciones, const std::string& path) {
 
         if(std::isdigit(static_cast<unsigned char>(first_char))){
             
-            std::stringstream ss(line);
+            std::stringstream line_stream(line);
             std::string num_str;
             // Transformamos la línea en stringstream
-            std::getline(ss, num_str, ':'); // Leemos el número de línea
+            std::getline(line_stream, num_str, ':'); // Leemos el número de línea
             int no_linea = std::stoi(num_str);
-            instrucciones[no_linea].no_linea = no_linea;
+            //instrucciones[no_linea].no_linea = no_linea;
             if (no_linea >= 0 && no_linea < INSTRUCCIONES_MAXIMO) {
                 instrucciones[no_linea].no_linea = no_linea;
 
                 std::string command_str;
-                ss >> command_str;
+                line_stream >> command_str;
                 instrucciones[no_linea].comando = stringToTipoComando(command_str, no_linea);
 
 
                 std::string operand_str;
-                ss >> operand_str;
+                line_stream >> operand_str;
 
                 separar_variables(instrucciones[no_linea].comando, instrucciones[no_linea].r, instrucciones[no_linea].s, instrucciones[no_linea].t, instrucciones[no_linea].d, operand_str, no_linea);
                 continue;
@@ -146,7 +179,8 @@ void lexer(Instruccion* instrucciones, const std::string& path) {
                 std::cerr << "Número de línea fuera de rango: " << no_linea << std::endl;
             }
         } else {
-            std::cout << "Error: la línea inicia con un caracter inválido: " << line << std::endl;
+            //std::cout << "Error: la línea inicia con un caracter inválido: " << line << std::endl;
+            std::cerr << "Error: la línea inicia con un caracter inválido: " << line << std::endl;
             std::exit(EXIT_FAILURE);
         }
     }
@@ -168,6 +202,10 @@ void ejecutar_codigo(Instruccion* instrucciones) {
                 reg[PC_REGISTRO]++;
                 std::cout << "? ";
                 std::cin >> reg[current_instruction.r];
+                if (std::cin.fail()) {
+                    std::cerr << "Error: el valor ingresado no es un número entero.\n";
+                    std::exit(EXIT_FAILURE);
+                }
                 break;
             case OUT:
                 reg[PC_REGISTRO]++;
@@ -189,7 +227,7 @@ void ejecutar_codigo(Instruccion* instrucciones) {
                 reg[PC_REGISTRO]++;
                 if (reg[current_instruction.t] == 0) {
                     std::cerr << "Error: División por cero en la instrucción en línea " << current_instruction.no_linea << std::endl;
-                    return;
+                    std::exit(EXIT_FAILURE);
                 }
                 reg[current_instruction.r] = reg[current_instruction.s] / reg[current_instruction.t];
                 break;
@@ -198,7 +236,7 @@ void ejecutar_codigo(Instruccion* instrucciones) {
                 a = current_instruction.d + reg[current_instruction.s];
                 if (a < 0 || a >= DATOS_MAXIMO) {
                     std::cerr << "Error: Acceso a memoria fuera de límites en la línea " << current_instruction.no_linea << std::endl;
-                    return;
+                    std::exit(EXIT_FAILURE);
                 }
                 reg[current_instruction.r] = datos_Memoria[a];
                 break;
@@ -215,7 +253,7 @@ void ejecutar_codigo(Instruccion* instrucciones) {
                 a = current_instruction.d + reg[current_instruction.s];
                 if (a < 0 || a >= DATOS_MAXIMO) {
                     std::cerr << "Error: Acceso a memoria fuera de límites en la línea " << current_instruction.no_linea << std::endl;
-                    return;
+                    std::exit(EXIT_FAILURE);
                 }
                 datos_Memoria[a] = reg[current_instruction.r];
                 break;
@@ -286,81 +324,96 @@ void ejecutar_codigo_debug(Instruccion* instrucciones) {
         // El switch ahora maneja la ejecución de la instrucción.
         switch (current_instruction.comando) {
             case IN:
+                reg[PC_REGISTRO]++;
                 imprimir_estado_maquina(current_instruction);
                 std::cout << "? ";
                 std::cin >> reg[current_instruction.r];
+                if (std::cin.fail()) {
+                    std::cerr << "Error: el valor ingresado no es un número entero.\n";
+                    std::exit(EXIT_FAILURE);
+                }
                 std::cout << "Presiona Enter para continuar..." << std::endl;
                 std::cin.get();
                 break;
             case OUT:
+                reg[PC_REGISTRO]++;
                 imprimir_estado_maquina(current_instruction);
                 std::cout << "> " << reg[current_instruction.r] << std::endl;
                 std::cout << "Presiona Enter para continuar..." << std::endl;
                 std::cin.get();
                 break;
             case ADD:
+                reg[PC_REGISTRO]++;
                 imprimir_estado_maquina(current_instruction);
                 reg[current_instruction.r] = reg[current_instruction.s] + reg[current_instruction.t];
                 std::cout << "Presiona Enter para continuar..." << std::endl;
                 std::cin.get();
                 break;
             case SUB:
+                reg[PC_REGISTRO]++;
                 imprimir_estado_maquina(current_instruction);
                 reg[current_instruction.r] = reg[current_instruction.s] - reg[current_instruction.t];
                 std::cout << "Presiona Enter para continuar..." << std::endl;
                 std::cin.get();
                 break;
             case MUL:
+                reg[PC_REGISTRO]++;
                 imprimir_estado_maquina(current_instruction);
                 reg[current_instruction.r] = reg[current_instruction.s] * reg[current_instruction.t];
                 std::cout << "Presiona Enter para continuar..." << std::endl;
                 std::cin.get();
                 break;
             case DIV:
+                reg[PC_REGISTRO]++;
                 imprimir_estado_maquina(current_instruction);
                 if (reg[current_instruction.t] == 0) {
                     std::cerr << "Error: División por cero en la instrucción en línea " << current_instruction.no_linea << std::endl;
-                    return;
+                    std::exit(EXIT_FAILURE);
                 }
                 reg[current_instruction.r] = reg[current_instruction.s] / reg[current_instruction.t];
                 std::cout << "Presiona Enter para continuar..." << std::endl;
                 std::cin.get();
                 break;
             case LD:
+                reg[PC_REGISTRO]++;
                 imprimir_estado_maquina(current_instruction);
                 a = current_instruction.d + reg[current_instruction.s];
                 if (a < 0 || a >= DATOS_MAXIMO) {
                     std::cerr << "Error: Acceso a memoria fuera de límites en la línea " << current_instruction.no_linea << std::endl;
-                    return;
+                    std::exit(EXIT_FAILURE);
                 }
                 reg[current_instruction.r] = datos_Memoria[a];
                 std::cout << "Presiona Enter para continuar..." << std::endl;
                 std::cin.get();
                 break;
             case LDA:
+                reg[PC_REGISTRO]++;
                 imprimir_estado_maquina(current_instruction);
                 reg[current_instruction.r] = current_instruction.d + reg[current_instruction.s];
                 std::cout << "Presiona Enter para continuar..." << std::endl;
                 std::cin.get();
                 break;
             case LDC:
+                reg[PC_REGISTRO]++;
                 imprimir_estado_maquina(current_instruction);
                 reg[current_instruction.r] = current_instruction.d;
                 std::cout << "Presiona Enter para continuar..." << std::endl;
                 std::cin.get();
                 break;
             case ST:
+                reg[PC_REGISTRO]++;
                 imprimir_estado_maquina(current_instruction);
                 a = current_instruction.d + reg[current_instruction.s];
                 if (a < 0 || a >= DATOS_MAXIMO) {
                     std::cerr << "Error: Acceso a memoria fuera de límites en la línea " << current_instruction.no_linea << std::endl;
-                    return;
+                    std::exit(EXIT_FAILURE);
                 }
                 datos_Memoria[a] = reg[current_instruction.r];
                 std::cout << "Presiona Enter para continuar..." << std::endl;
                 std::cin.get();
                 break;
             case JLT:
+                reg[PC_REGISTRO]++;
                 imprimir_estado_maquina(current_instruction);
                 if (reg[current_instruction.r] < 0) {
                     //reg[PC_REGISTRO] = current_instruction.d;
@@ -371,6 +424,7 @@ void ejecutar_codigo_debug(Instruccion* instrucciones) {
                 std::cin.get();
                 break;
             case JLE:
+                reg[PC_REGISTRO]++;
                 imprimir_estado_maquina(current_instruction);
                 if (reg[current_instruction.r] <= 0) {
                     //reg[PC_REGISTRO] = current_instruction.d;
@@ -381,6 +435,7 @@ void ejecutar_codigo_debug(Instruccion* instrucciones) {
                 std::cin.get();
                 break;
             case JGE:
+                reg[PC_REGISTRO]++;
                 imprimir_estado_maquina(current_instruction);
                 if (reg[current_instruction.r] >= 0) {
                     //reg[PC_REGISTRO] = current_instruction.d;
@@ -391,6 +446,7 @@ void ejecutar_codigo_debug(Instruccion* instrucciones) {
                 std::cin.get();
                 break;
             case JGT:
+                reg[PC_REGISTRO]++;
                 imprimir_estado_maquina(current_instruction);
                 if (reg[current_instruction.r] > 0) { 
 
@@ -403,6 +459,7 @@ void ejecutar_codigo_debug(Instruccion* instrucciones) {
                 std::cin.get();
                 break;
             case JEQ:
+                reg[PC_REGISTRO]++;
                 imprimir_estado_maquina(current_instruction);
                 if (reg[current_instruction.r] == 0) {
                     reg[PC_REGISTRO] = current_instruction.d + reg[current_instruction.s];
@@ -412,6 +469,7 @@ void ejecutar_codigo_debug(Instruccion* instrucciones) {
                 std::cin.get();
                 break;
             case JNE:
+                reg[PC_REGISTRO]++;
                 imprimir_estado_maquina(current_instruction);
                 if (reg[current_instruction.r] != 0) {
                     reg[PC_REGISTRO] = current_instruction.d + reg[current_instruction.s];
@@ -422,14 +480,12 @@ void ejecutar_codigo_debug(Instruccion* instrucciones) {
                 std::cin.get();
                 break;
         }
-
         // Si no se ejecutó un salto, incrementamos el PC para pasar a la siguiente instrucción.
-        reg[PC_REGISTRO]++;
     }
 }
 
 void imprimir_estado_maquina(Instruccion instruccion_actual) {
-    std::cout << "Instrucción actual (línea " << instruccion_actual.no_linea << "): ";
+    std::cout << "Instrucción actual: \n" << instruccion_actual.no_linea << ": ";
     switch (instruccion_actual.comando) {
         case HALT: std::cout << "HALT"; break;
         case IN: std::cout << "IN"; break;
@@ -449,15 +505,14 @@ void imprimir_estado_maquina(Instruccion instruccion_actual) {
         case JEQ: std::cout << "JEQ"; break;
         case JNE: std::cout << "JNE"; break;
     }
-    std::cout << " Operandos (r=" << instruccion_actual.r 
-              << ", s=" << instruccion_actual.s 
-              << ", t=" << instruccion_actual.t 
-              << ", d=" << instruccion_actual.d 
-              << ")" << std::endl;
+    std::cout << "\nr: " << instruccion_actual.r 
+              << "\ns: " << instruccion_actual.s 
+              << "\nt: " << instruccion_actual.t 
+              << "\nd: " << instruccion_actual.d << std::endl;
 
-    std::cout << "Registros: ";
+    std::cout << "================= \nRegistros: \n";
     for (int i = 0; i < NUMERO_REGISTROS; ++i) {
-        std::cout << "R" << i << "=" << reg[i] << " ";
+        std::cout << "R[" << i << "] = " << reg[i] << std::endl;
     }
     std::cout << std::endl;
 
